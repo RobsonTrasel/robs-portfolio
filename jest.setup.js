e8b72893f40e643cee6b662752627794213b0a94
RobsonTrasel <robson.trasel@gmail.com>
2022-11-16 22:47:44 -0300

    chore: jest config done

---
+++ jest.setup.js
@@ -0,0 +1,21 @@
+import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/react';
+
+window.matchMedia =
+	window.matchMedia ||
+	function () {
+		return {
+			matches: false,
+			addListener: function () {},
+			removeListener: function () {},
+		};
+	};
+
+jest.mock('Atoms/Icon', () => {
+	return {
+		__esModule: true,
+		Icon: () => {
+			return <svg />;
+		},
+	};
+});
