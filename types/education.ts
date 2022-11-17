e945e9ea4641fc9b8121cea2164bc7c637876e59
RobsonTrasel <robson.trasel@gmail.com>
2022-11-16 23:18:13 -0300

    chores: types done

---
+++ types/education.ts
@@ -0,0 +1,15 @@
+import { ICompany, IRawCompany } from 'types';
+
+export interface IEducation {
+	institute: ICompany;
+	course: string;
+	date: string;
+	technologies: string[];
+}
+
+export interface IRawEducation {
+	course: string;
+	date: string;
+	institute: IRawCompany;
+	courseContents: Array<{ skill: string }>;
+}
