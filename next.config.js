04ad6c2fc2cefda0a0c1059fb7446d3aa16b30a9
RobsonTrasel <robson.trasel@gmail.com>
2022-11-16 21:06:18 -0300

    fix: next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,33 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+const { withSentryConfig } = require('@sentry/nextjs')
+
+const moduleExports = {
+  poweredByHeader: false,
+	swcMinify: true,
+	productionBrowserSourceMaps: process.env.NODE_ENV === 'production',
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: ['@svgr/webpack'],
+		});
+
+		return config;
+	},
+	publicRuntimeConfig: {
+		MAILCHIMP_API_KEY: process.env.MAILCHIMP_API_KEY || '',
+		MAILCHIMP_API_SERVER: process.env.MAILCHIMP_API_SERVER || '',
+		MAILCHIMP_AUDIENCE_ID: process.env.MAILCHIMP_AUDIENCE_ID || '',
+	},
+	images: {
+		domains: [
+			'dev-to-uploads.s3.amazonaws.com',
+			'media.graphcms.com',
+			'media.graphassets.com',
+		],
+	},
 }
 
-module.exports = nextConfig
-  
+const sentryWebpackPluginOptions = {
+  silent: false
+}
+
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions)
